290bf00a65e01a79bc83f0be8d606b3eb2c14fc2
Diogo Carasco <carasco@gmail.com>
2024-12-27 09:56:55 -0300

    first commit for assessment

--- app/Services/AffiliateService.php
+++ app/Services/AffiliateService.php
@@ -9,6 +9,8 @@ use App\Models\Merchant;
 use App\Models\Order;
 use App\Models\User;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
+
 
 class AffiliateService
 {
@@ -27,6 +29,46 @@ class AffiliateService
      */
     public function register(Merchant $merchant, string $email, string $name, float $commissionRate): Affiliate
     {
-        // TODO: Complete this method
+
+        DB::beginTransaction();
+
+        try {
+
+            if ($this->checkEmailAvailability($email)) {
+                throw new AffiliateCreateException('Email already in use');
+            }
+
+            $user = User::create([
+                'email' => $email,
+                'name' => $name,
+                'type' => User::TYPE_AFFILIATE,
+            ]);
+        
+            $discountCode = $this->apiService->createDiscountCode($merchant)['code'];
+
+            $affiliate = Affiliate::create([
+                'merchant_id' => $merchant->id,
+                'user_id' => $user->id,
+                'commission_rate' => $commissionRate,
+                'discount_code' => $discountCode,
+            ]);
+        
+            Mail::to($email)->send(new AffiliateCreated($affiliate));
+        
+            DB::commit();
+        
+            return $affiliate;
+        
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e; 
+        }
     }
+
+    public function checkEmailAvailability(string $email): bool
+    {
+        return User::where('email', $email)->exists();
+    }
+
+
 }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -6,6 +6,8 @@ use App\Models\Affiliate;
 use App\Models\Merchant;
 use App\Models\Order;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class OrderService
 {
@@ -23,6 +25,46 @@ class OrderService
      */
     public function processOrder(array $data)
     {
-        // TODO: Complete this method
+        $order = Order::firstOrNew(['external_order_id' => $data['order_id']]);
+        if ($order->exists) {
+            Log::info("Order with ID {$data['order_id']} already exists. Skipping.");
+            return;
+        }
+
+        $merchant = Merchant::where('domain', $data['merchant_domain'])->first();
+        //$affiliate = $this->affiliateService->register($merchant, $data['customer_email'], $data['customer_name'],0.1);
+        $affiliate = Affiliate::first();
+        $commission = floatval($data['subtotal_price']) * $affiliate->commission_rate;
+        $order = new Order([
+            'external_order_id' => $data['order_id'],
+            'subtotal' => $data['subtotal_price'],
+            'discount_code' => $data['discount_code'],
+            'customer_email' => $data['customer_email'],
+            'commission_owed' => $commission,
+            'merchant_id' => $merchant->id,
+            'affiliate_id' => $affiliate->id,
+            'payout_status' => Order::STATUS_UNPAID
+        ]);
+
+        $order->save();
+
+        $this->logCommission($order);
+    }
+
+    /**
+     * Log the commission for an order.
+     *
+     * @param  Order  $order
+     * @return void
+     */
+    public function logCommission(Order $order)
+    {
+        $commissionRate = $order->merchant->commission_rate;
+        $commissionAmount = $order->subtotal * $commissionRate;
+
+        $order->commission_owed = $commissionAmount;
+        $order->save();
+
+        Log::info("Commission of ". $commissionAmount. " for order ".$order->order_id);
     }
 }
